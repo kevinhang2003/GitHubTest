91df4b41f01c4d2acf59b82fc0aeb035b9f15dcc
Huang Sheng Hsin <kevinhang2003@gmail.com>
2020-06-26 21:36:11 +0800

    Initialise files

---
+++ GitHubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubTest
+//
+//  Created by YuTech x CBIC on 2020/6/26.
+//  Copyright © 2020 YuTech x CBIC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
